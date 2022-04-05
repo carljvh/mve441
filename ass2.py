e50e87ea652728ee53cdadd437d6fe8521859773
Martin <49361682+mabergst@users.noreply.github.com>
2022-04-05 11:57:28 +0200

    Added quad and mvn generation
    numbers looking weird...

--- ass2.py
+++ ass2.py
@@ -1,9 +1,13 @@
+from black import T
 from sklearn import tree
 import numpy as np
 import pandas as pd
 import random
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.metrics import zero_one_loss
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+
 
 def generate_uni_data(n, x1_lim, x2_lim):
     X = np.zeros([n,2])
@@ -17,6 +21,23 @@ def generate_uni_data(n, x1_lim, x2_lim):
         #  else:  #Not needed
         #     y[i] = 0
     return X, y
+
+    # mu, sigma vectors with length of classes
+def generate_gaussian_data(n, mu, sigma):
+    X = np.random.normal(mu, sigma, [n,2])
+    y = np.zeros(n)
+    y[int(n/2):] = 1
+    return X, y
+
+ # each class should have individual covar matrix
+def generate_mvn_data(n, mu, covar):
+    #X = np.random.multivariate_normal(mu, covar, [n,2])
+    X = np.random.multivariate_normal(mean=[5,5], cov=covar[0], size=int(n/2))
+    X = np.append(X,np.random.multivariate_normal(mean=[5,5], cov=covar[1], size=int(n/2)),axis=0)
+    y = np.zeros(n)
+    y[int(n/2):] = 1
+    return X, y
+
 
 
 def evaluate(n_datapoints: int, runs: int):
@@ -36,8 +57,41 @@ def evaluate(n_datapoints: int, runs: int):
     
     return np.mean(accuracy), np.std(accuracy)
 
+def evaluateQDA(n_datapoints: int, runs: int, mean, covar):
+    accuracy = np.zeros(runs)
+    loss = np.zeros(runs)
+    for i in range(runs):
+        data = generate_mvn_data(n, mean, covar)
+        #data = generate_gaussian_data(n,5,4)
+        #print(data)
+
+        X = data[0]
+        y = data[1]
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+        qda = QuadraticDiscriminantAnalysis()
+        qdaFit = qda.fit(X_train,y_train)
+
+        y_pred = qdaFit.predict(X_test)
+        accuracy[i] = accuracy_score(y_test, y_pred)
+
+        y_g_train_pred = qda.predict(X_train)
+        loss[i] = zero_one_loss(y_train, y_g_train_pred)
+       
+    
+    return np.mean(accuracy), np.std(accuracy), np.mean(loss) 
+
+
+
+mu = [5,5]
+covar = [[[1,0],[0,1]],  [[2,1],[1,2]]]
 
 n = 100
 runs = 10
-result = evaluate(n, runs)
+
+
+
+result = evaluateQDA(n, runs, mu, covar)
 print("mean accuracy: %f (std %f)" % (result[0],result[1]))
+print(f'Training Loss = {result[2]}')
